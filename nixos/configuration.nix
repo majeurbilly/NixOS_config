57457add76ef369bec89ec624765217aa29a7f5c
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2025-07-25 20:30:40 -0400

    remise par default

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running nixos-help.).
 
 { config, pkgs, ... }:
 
@@ -65,10 +65,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nixos01 = {
+  # Define a user account. Don't forget to set a password with passwd.
+  users.users.g2k4 = {
     isNormalUser = true;
-    description = "nixos-01";
+    description = "g2k4";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
     #  thunderbird
@@ -83,8 +83,6 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    pkgs.git
-    minecraft
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -108,19 +106,10 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
-
-  boot.loader.grub.extraEntries = ''
-    menuentry "Minecraft Launcher" {
-      linux /boot/vmlinuz-linux
-      initrd /boot/initramfs-linux.img
-      options root=/dev/sdX quiet
-      # Ici tu pourrais appeler le script que tu as créé
-    }
-  '';
 }
