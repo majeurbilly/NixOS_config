6384b4f6194bb76a6830d1ca837ea383696c7522
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2025-07-25 20:37:48 -0400

    fr

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,52 +1,52 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running nixos-help.).
+# Modifiez ce fichier de configuration pour définir ce qui doit être installé sur
+# votre système. De l’aide est disponible dans la page de manuel configuration.nix(5)
+# et dans le manuel de NixOS (accessible via la commande nixos-help).
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ # Inclut les résultats de l’analyse matérielle.
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos"; # Définir le nom d’hôte de votre machine.
+  # networking.wireless.enable = true;  # Active le support sans fil via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configurez un proxy réseau si nécessaire
+  # networking.proxy.default = "http://utilisateur:motdepasse@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,domaine.interne";
 
-  # Enable networking
+  # Active le réseau
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Définir le fuseau horaire.
   time.timeZone = "America/Toronto";
 
-  # Select internationalisation properties.
+  # Sélectionner les propriétés de langue.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable the X11 windowing system.
+  # Activer le système de fenêtrage X11.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Activer l’environnement de bureau GNOME.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  # Configurer la disposition du clavier dans X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
+  # Activer CUPS pour permettre l’impression de documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Activer le son avec PipeWire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -54,18 +54,18 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
+    # Si vous voulez utiliser des applications JACK, décommentez cette ligne
     #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
+    # Utilise le gestionnaire de session par défaut (le seul disponible pour le moment),
+    # inutile de le redéfinir dans votre configuration pour l’instant.
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
+  # Activer le pavé tactile (activé par défaut dans la plupart des environnements de bureau).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with passwd.
+  # Définir un compte utilisateur. N’oubliez pas de définir un mot de passe avec la commande `passwd`.
   users.users.g2k4 = {
     isNormalUser = true;
     description = "g2k4";
@@ -75,41 +75,40 @@
     ];
   };
 
-  # Install firefox.
+  # Installer Firefox.
   programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
+  # Lister les paquets installés dans le profil système. Pour rechercher un paquet, utilisez :
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  vim # N’oubliez pas d’ajouter un éditeur pour modifier ce fichier ! Nano est installé par défaut.
   #  wget
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+  # Certains programmes ont besoin de droits SUID, peuvent être configurés plus finement ou
+  # sont lancés dans les sessions utilisateur.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+  # Liste des services à activer :
 
-  # Enable the OpenSSH daemon.
+  # Activer le service OpenSSH.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
+  # Ouvrir des ports dans le pare-feu.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
+  # Ou désactiver complètement le pare-feu.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  # Cette valeur détermine la version de NixOS à partir de laquelle les réglages
+  # par défaut (comme les emplacements des fichiers ou versions de base de données)
+  # ont été appliqués à votre système. Il est recommandé de laisser cette valeur
+  # à celle de votre installation initiale.
+  # Avant de la modifier, lisez la documentation de cette option
+  # (ex. man configuration.nix ou https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Avez-vous lu le commentaire ?
 }
